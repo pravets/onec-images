8c27a1a314b4b218624a69b700776931ddedf7dd
Iosif Pravets <i@pravets.ru>
2025-08-10 22:13:24 +0000

    feat(platform,ci): добавить поддержку 8.3.23 и учесть её в CI
    - Добавлен `src/onec-platform/8.3.23.Dockerfile`
    - В `src/build-onec-platform.sh` расширен диапазон поддерживаемых версий до 8.3.23–8.3.27
    - В `.github/workflows/ci-onec-platform.yml` добавлено отслеживание 8.3.23 и тест конкретной версии 8.3.23.2236

--- src/build-onec-platform.sh
+++ src/build-onec-platform.sh
@@ -51,7 +51,7 @@ DOCKER_BUILDKIT=1 docker build \
         version_prefix=$(echo "$ONEC_VERSION" | awk -F. '{print $1 "." $2 "." $3}')
         dockerfile_path="${SCRIPT_DIR}/../src/onec-platform/${version_prefix}.Dockerfile"
         if [[ ! -f "$dockerfile_path" ]]; then
-            log_failure "Не найден подходящий Dockerfile для версии ${version_prefix}. Поддерживаются 8.3.24–8.3.27"
+            log_failure "Не найден подходящий Dockerfile для версии ${version_prefix}. Поддерживаются 8.3.23–8.3.27"
             exit 1
         fi
         echo "$dockerfile_path"
